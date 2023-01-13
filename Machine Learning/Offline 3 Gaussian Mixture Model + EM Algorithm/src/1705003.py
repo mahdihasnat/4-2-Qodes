ed20ed23beff2d6fe125ba493b617683abf1d8e5
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-01-13 19:05:25 +0600

    kind a working

--- Machine Learning/Offline 3 Gaussian Mixture Model + EM Algorithm/src/1705003.py
+++ Machine Learning/Offline 3 Gaussian Mixture Model + EM Algorithm/src/1705003.py
@@ -2,16 +2,22 @@ import numpy as np
 from data_handler import load_dataset
 from mixture_model import GMM
 
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('fivethirtyeight')
+import numpy as np
+from scipy.stats import norm
+
 def main():
     x = load_dataset()
     
     print("type x = ", type(x))
     print("shape x = ", x.shape)
     
-    for k in range(3,4):
+    for k in range(1,10):
         params = {}
         params['k'] = k
-        params['max_iter'] = 100
+        params['max_iter'] = 20
         params['tol'] = 1e-6
         g = GMM(**params)
         g.fit(x)
--- Machine Learning/Offline 3 Gaussian Mixture Model + EM Algorithm/src/mixture_model.py
+++ Machine Learning/Offline 3 Gaussian Mixture Model + EM Algorithm/src/mixture_model.py
@@ -79,7 +79,7 @@ class GMM:
         for i in range(self.max_iter):
             self.e_step(X)
             self.m_step(X)
-            if i % 2 == 0:
+            if i % 5 == 0:
                 # print("mean = ", self.mu)
                 # print("sigma = ", self.sigma)
                 print("iter = ", i, "log_likelihood = ", self.log_likelihood(X))
