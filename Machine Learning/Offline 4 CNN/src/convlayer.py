3c61e41bd72d11a87f2e109be6fcbb0d625d75e3
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-02-06 03:14:51 +0600

    added forward

--- Machine Learning/Offline 4 CNN/src/convlayer.py
+++ Machine Learning/Offline 4 CNN/src/convlayer.py
@@ -45,28 +45,41 @@ class ConvLayer:
 
         
         # bias shape = (out_channels)
-        self.bias = np.random.randn(out_channels)\
+        self.biases = np.random.randn(out_channels)\
                 *np.sqrt(2/(in_channels*self.kernel_shape[0]*self.kernel_shape[1]))
         
         assert self.weights.shape == (out_channels, in_channels, self.kernel_shape[0], self.kernel_shape[1]) , "wight shape dont match"
-        assert self.bias.shape == (out_channels,) , "bias shape dont match"
+        assert self.biases.shape == (out_channels,) , "bias shape dont match"
     
     
     def forward(self,x):
         """
-            x: shape = (batch_size, in_channels, height, width) or (in_channels, height, width)
-            
+            x: shape = (batch_size, in_channels, height, width)
         """
-        x_shape = x.shape[-3:]
-        print("x_shape: ",x_shape)
-        out_shape =( (x_shape[1] + 2*self.padding[0] - self.kernel_shape[0] +1)//self.stride[0],\
-                    (x_shape[2] + 2*self.padding[1] - self.kernel_shape[1] +1)//self.stride[1] )
+        assert len(x.shape) == 4, "input shape is not 4D"
+        assert x.shape[1] == self.in_channels, "input channel dont match"
+        
+        print("x_shape: ",x.shape)
+        out_shape =( (x.shape[2] + 2*self.padding[0] - self.kernel_shape[0] +1)//self.stride[0],\
+                    (x.shape[3] + 2*self.padding[1] - self.kernel_shape[1] +1)//self.stride[1] )
         print("out_shape: ",out_shape)
         
         assert out_shape[0] > 0 and out_shape[1] > 0, "output shape is negative"
         
+        # https://numpy.org/doc/stable/reference/generated/numpy.pad.html
+        padded_x = np.pad(x , ((0,0),(0,0),(self.padding[0],self.padding[0]),(self.padding[1],self.padding[1])),\
+                            mode='constant', constant_values=0)
+        print("padded_x: ",padded_x)
         
-        
+        out_x = np.zeros((x.shape[0],self.out_channels,out_shape[0],out_shape[1]))
+        for i in range(x.shape[0]):
+            for j in range(self.out_channels):
+                for k in range(out_shape[0]):
+                    for l in range(out_shape[1]):
+                        out_x[i,j,k,l] = np.sum(padded_x[i,:,k*self.stride[0]:k*self.stride[0]+self.kernel_shape[0],\
+                                                        l*self.stride[1]:l*self.stride[1]+self.kernel_shape[1]]\
+                                                *self.weights[j,:,:,:]) + self.biases[j]
+        return out_x
 
 
 import torch.nn as nn
@@ -75,7 +88,7 @@ if __name__ == '__main__':
     # Test for My convolutional layer using Pytorch convolutional layer
     
     m = nn.Conv2d(1, 2, 2, stride=1,dtype=torch.float64)
-    my = ConvLayer(1, 2, 2, stride=1,padding=0)
+    my = ConvLayer(1, 2, 2, stride=1,padding=1)
     x = np.array([.1,.2,.3,.4,.5,.6,.7,.8,.9]).reshape(1,1,3,3)
     print("x: ",x)
     print("x.shape: ",x.shape)
