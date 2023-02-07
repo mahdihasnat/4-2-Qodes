68d276a04251d2394c777dd72943d22ae84137cc
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-02-07 21:40:44 +0600

    added simple soln

---
+++ Simulation and Modeling/Offline 5 Monte Carlo Simulation/src/1705003-task1.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def sample(n,m,s)->bool:
+    perm = np.random.permutation(n)
+    selected = None
+    if m == 0:
+        selected = perm[0]
+    else:
+        mn = np.min(perm[:m])
+        for i in range(m,n):
+            if mn>perm[i]:
+                selected = perm[i]
+                break
+    if selected is None:
+        selected = perm[-1]
+    return selected<s
+
+def prob(n,m,s,iter):
+    tot=0
+    for _ in range(iter):
+        if sample(n,m,s):
+            tot +=1
+    return tot/iter
+
+n=100
+s=1
+iter=1000000
+x = np.arange(0,n)
+y = np.array([prob(n,m,s,iter) for m in x])
+plt.plot(x,y)
+plt.title("n={} s={} iteration={}".format(n,s,iter))
+plt.xlabel("m")
+plt.ylabel("P")
+plt.show()
